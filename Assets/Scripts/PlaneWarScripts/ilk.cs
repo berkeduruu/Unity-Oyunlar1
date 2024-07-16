c2cd99019e7ce8dd81cf33ab9cc8c4b1cd7c3797
yagizyungul <yagizyy12@gmail.com>
2024-07-16 17:28:02 +0300

    dasd

---
+++ Assets/Scripts/PlaneWarScripts/ilk.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ilk : MonoBehaviour
+{
+    public string AD; 
+    void Start()
+    {
+        Debug.Log(AD);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
